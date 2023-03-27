49a9cd2cbe05eeefb5b183e9d659354f3e236912
dwibb <dwib031@gmail.com>
2023-03-27 16:45:45 +0700

    Commit pertama

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var menub = document.getElementsByClassName('menub')[0];
 var menuc = document.getElementsByClassName('menuc')[0];
+var call = document.getElementsByClassName('call')[0];
+var contcall = document.getElementsByClassName('contcall')[0];
 
 menub.onclick = function() {
     menuc.classList.toggle('active');
+}
+contcall.onclick = function() {
+    call.classList.toggle('callactive');
 }
